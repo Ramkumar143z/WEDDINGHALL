b1a5b85880c650e547639419f8738a4e1859a632
Ramkumar143z <ramcse142@gmail.com>
2026-06-23 21:51:09 +0530

    Push to new repository

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 git add .
-git commit -m "Initial commit"
-git remote add origin https://github.com/Ramkumar143z/WEDDINGHALL.git
+git commit -m "Push to new repository"
+git remote set-url origin https://github.com/skydigital34/AARAV-WEEDDING-HALL.git 2>nul || git remote add origin https://github.com/skydigital34/AARAV-WEEDDING-HALL.git
 git branch -M main
 git push -u origin main
