0a43069b1671c8468e7b87609640d55cba391719
Ramkumar143z <ramcse142@gmail.com>
2026-06-23 21:55:49 +0530

    Push to wedding hall repository

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 git add .
-git commit -m "Push to new repository"
-git remote set-url origin https://github.com/skydigital34/AARAV-WEEDDING-HALL.git 2>nul || git remote add origin https://github.com/skydigital34/AARAV-WEEDDING-HALL.git
+git commit -m "Push to wedding hall repository"
+git remote set-url origin https://github.com/Ramkumar143z/WEDDINGHALL.git 2>nul || git remote add origin https://github.com/Ramkumar143z/WEDDINGHALL.git
 git branch -M main
 git push -u origin main
